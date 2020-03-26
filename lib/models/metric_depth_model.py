5fb9f13c3582991ad540607844b01427ee41a9d6
YvanYin <yvanwy@outlook.com>
2020-03-26 23:32:39 +1030

    add

--- lib/models/metric_depth_model.py
+++ lib/models/metric_depth_model.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 import numpy as np
 from . import lateral_net as lateral_net
 from lib.utils.net_tools import get_func
+from lib.models.WCEL_loss import WCEL_Loss
+from lib.models.VNL_loss import VNL_Loss
 from lib.models.image_transfer import bins_to_depth, kitti_merge_imgs
 from lib.core.config import cfg
 
@@ -22,9 +24,17 @@ class MetricDepthModel(nn.Module):
     def inference(self, data):
         with torch.no_grad():
             out = self.forward(data)
-            pred_depth = bins_to_depth(out['b_fake_softmax'])
-            b_fake = pred_depth
-            return {'b_fake': b_fake}
+
+            if cfg.MODEL.PREDICTION_METHOD == 'classification':
+                pred_depth = bins_to_depth(out['b_fake_softmax'])
+            elif cfg.MODEL.PREDICTION_METHOD == 'regression':
+                # for regression methods
+                pred_depth = torch.nn.functional.sigmoid(out['b_fake_logit'])
+            else:
+                raise ValueError("Unknown prediction methods")
+
+            out = pred_depth
+            return {'b_fake': out}
 
     def inference_kitti(self, data):
         #crop kitti images into 3 parts
@@ -43,6 +53,64 @@ class MetricDepthModel(nn.Module):
 
             out = kitti_merge_imgs(self.b_l_fake, self.b_m_fake, self.b_r_fake, torch.squeeze(data['B_raw']).shape, data['crop_lmr'])
             return {'b_fake': out}
+
+
+class ModelLoss(object):
+    def __init__(self):
+        super(ModelLoss, self).__init__()
+        self.weight_cross_entropy_loss = WCEL_Loss()
+        self.virtual_normal_loss = VNL_Loss(focal_x=cfg.DATASET.FOCAL_X, focal_y=cfg.DATASET.FOCAL_Y, input_size=cfg.DATASET.CROP_SIZE)
+
+    def criterion(self, pred_softmax, pred_logit, data, epoch):
+        pred_depth = bins_to_depth(pred_softmax)
+        loss_metric = self.weight_cross_entropy_loss(pred_logit, data['B_bins'], data['B'].cuda())
+        loss_normal = self.virtual_normal_loss(data['B'].cuda(), pred_depth)
+
+        loss = {}
+        loss['metric_loss'] = loss_metric
+        loss['virtual_normal_loss'] = cfg.MODEL.DIFF_LOSS_WEIGHT * loss_normal
+        loss['total_loss'] = loss['metric_loss'] + loss['virtual_normal_loss']
+        return loss
+
+
+class ModelOptimizer(object):
+    def __init__(self, model):
+        super(ModelOptimizer, self).__init__()
+        encoder_params = []
+        encoder_params_names = []
+        decoder_params = []
+        decoder_params_names = []
+        nograd_param_names = []
+
+        for key, value in dict(model.named_parameters()).items():
+            if value.requires_grad:
+                if 'res' in key:
+                    encoder_params.append(value)
+                    encoder_params_names.append(key)
+                else:
+                    decoder_params.append(value)
+                    decoder_params_names.append(key)
+            else:
+                nograd_param_names.append(key)
+
+        lr_encoder = cfg.TRAIN.BASE_LR
+        lr_decoder = cfg.TRAIN.BASE_LR * cfg.TRAIN.SCALE_DECODER_LR
+        weight_decay = 0.0005
+
+        net_params = [
+            {'params': encoder_params,
+             'lr': lr_encoder,
+             'weight_decay': weight_decay},
+            {'params': decoder_params,
+             'lr': lr_decoder,
+             'weight_decay': weight_decay},
+            ]
+        self.optimizer = torch.optim.SGD(net_params, momentum=0.9)
+    def optim(self, loss):
+        self.optimizer.zero_grad()
+        loss_all = loss['total_loss']
+        loss_all.backward()
+        self.optimizer.step()
 
 
 class DepthModel(nn.Module):
